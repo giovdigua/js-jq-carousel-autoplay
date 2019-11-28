5ab8d7a03869e7e3d71370d798c2fb0f32352244
giovdigua <giovdigua@gmail.com>
2019-11-28 18:40:19 +0100

    Minor chage

--- main.js
+++ main.js
@@ -11,7 +11,6 @@ $(document).ready(function() {
         var currentBullet = $('.bullet.select');
         var nextBullet;
 
-
         if (current.is(index.last())) {
             next = index.first();
         }else{
@@ -31,70 +30,6 @@ $(document).ready(function() {
         nextBullet.addClass('select');
     },3000);
 
-    // //Prec
-    // $('#leftContainer').click(function() {
-    //
-    //     //Variables for images
-    //     var index = $('img');
-    //     var current = $('img.active');
-    //     var prec;
-    //     //Variables for the bullets
-    //     var bulletIndex = $('.bullet');
-    //     var currentBullet = $('.bullet.select');
-    //     var precBullet;
-    //
-    //     if (current.is(index.first())) {
-    //         prec = index.last();
-    //     }else{
-    //         prec = current.prev('img');
-    //
-    //     }
-    //
-    //     if (currentBullet.is(bulletIndex.first())) {
-    //         precBullet = bulletIndex.last();
-    //     } else {
-    //         precBullet = currentBullet.prev('.bullet');
-    //     }
-    //
-    //     current.removeClass('active');
-    //     prec.addClass('active');
-    //     currentBullet.removeClass('select');
-    //     precBullet.addClass('select');
-    //
-    // });
-    // //next
-    // $('#rightContainer').click(function() {
-    //
-    //     //Variables for images
-    //     var index = $('img');
-    //     var current = $('img.active');
-    //     var next;
-    //     //Variables for the bullets
-    //     var bulletIndex = $('.bullet');
-    //     var currentBullet = $('.bullet.select');
-    //     var nextBullet;
-    //
-    //
-    //     if (current.is(index.last())) {
-    //         next = index.first();
-    //     }else{
-    //         next = current.next('img');
-    //
-    //     }
-    //
-    //     if (currentBullet.is(bulletIndex.last())) {
-    //         nextBullet = bulletIndex.first();
-    //     } else {
-    //         nextBullet = currentBullet.next('.bullet');
-    //     }
-    //
-    //     current.removeClass('active');
-    //     next.addClass('active');
-    //     currentBullet.removeClass('select');
-    //     nextBullet.addClass('select');
-    //
-    // });
-
     //this function is the BONUS challenge
     //With this you can change the img click on bullet
     $('.bullet').click(function () {
@@ -106,8 +41,6 @@ $(document).ready(function() {
             //$('#sliderIMGContainer').children().eq(indice).addClass('active');
             $('#sliderIMGContainer img').eq(indice).addClass('active');
     });
-
-
 
     //New implementations
     function interceptClick( event ) {
