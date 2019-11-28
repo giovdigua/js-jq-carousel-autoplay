31354406765661cd13b523872fb0afb804687fb7
giovdigua <giovdigua@gmail.com>
2019-11-28 18:29:52 +0100

    Fix bug

--- main.js
+++ main.js
@@ -31,6 +31,70 @@ $(document).ready(function() {
         nextBullet.addClass('select');
     },3000);
 
+    // //Prec
+    // $('#leftContainer').click(function() {
+    //
+    //     //Variables for images
+    //     var index = $('img');
+    //     var current = $('img.active');
+    //     var prec;
+    //     //Variables for the bullets
+    //     var bulletIndex = $('.bullet');
+    //     var currentBullet = $('.bullet.select');
+    //     var precBullet;
+    //
+    //     if (current.is(index.first())) {
+    //         prec = index.last();
+    //     }else{
+    //         prec = current.prev('img');
+    //
+    //     }
+    //
+    //     if (currentBullet.is(bulletIndex.first())) {
+    //         precBullet = bulletIndex.last();
+    //     } else {
+    //         precBullet = currentBullet.prev('.bullet');
+    //     }
+    //
+    //     current.removeClass('active');
+    //     prec.addClass('active');
+    //     currentBullet.removeClass('select');
+    //     precBullet.addClass('select');
+    //
+    // });
+    // //next
+    // $('#rightContainer').click(function() {
+    //
+    //     //Variables for images
+    //     var index = $('img');
+    //     var current = $('img.active');
+    //     var next;
+    //     //Variables for the bullets
+    //     var bulletIndex = $('.bullet');
+    //     var currentBullet = $('.bullet.select');
+    //     var nextBullet;
+    //
+    //
+    //     if (current.is(index.last())) {
+    //         next = index.first();
+    //     }else{
+    //         next = current.next('img');
+    //
+    //     }
+    //
+    //     if (currentBullet.is(bulletIndex.last())) {
+    //         nextBullet = bulletIndex.first();
+    //     } else {
+    //         nextBullet = currentBullet.next('.bullet');
+    //     }
+    //
+    //     current.removeClass('active');
+    //     next.addClass('active');
+    //     currentBullet.removeClass('select');
+    //     nextBullet.addClass('select');
+    //
+    // });
+
     //this function is the BONUS challenge
     //With this you can change the img click on bullet
     $('.bullet').click(function () {
@@ -73,8 +137,7 @@ $(document).ready(function() {
                 nextBullet = currentBullet.next('.bullet');
             }
 
-        }
-        if ( target.is( "#leftContainer i" ) ) {
+        }else if ( target.is( "#leftContainer i" ) ) {
                 console.log('Sx');
                 if (current.is(index.first())) {
                     next = index.last();
@@ -88,6 +151,8 @@ $(document).ready(function() {
                 } else {
                     nextBullet = currentBullet.prev('.bullet');
                 }
+        }else{
+            return;
         }
 
         current.removeClass('active');
